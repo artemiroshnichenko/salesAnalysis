d348e608ce6623f2295778de2acf4e5da57302b6
artemiroshnichenko <artemiroshnichenko@gmail.com>
2021-05-10 12:55:49 -0400

    GA add request body

--- get_data.py
+++ get_data.py
@@ -17,26 +17,49 @@ class googleAnalitics():
 
     def initialize_analyticsreporting(self):
         """Initializes an Analytics Reporting API V4 service object.
-
         Returns:
             An authorized Analytics Reporting API V4 service object.
         """
         credentials = ServiceAccountCredentials.from_json_keyfile_name(
             self.KEY_FILE_LOCATION, self.SCOPES)
-
         # Build the service object.
         self.analytics = build('analyticsreporting', 'v4', credentials=credentials)
 
-    def get_report(self, body):
-        """Queries the Analytics Reporting API V4.
+    def request_body(self, DIMS, METRICS, START_DATE='today', END_DATE='today'):
+        """Create request list for report
+        Args:
+            DIMS: List of dimensions
+            METRICS: List of metrics
+            START_DATE: Start date for report
+            END_DATE: End date for report
+        Returns:
+            Request body
+        """
+        self.requests_list =  [{
+                'viewId': self.VIEW_ID,
+                'dateRanges': [{'startDate': START_DATE, 'endDate': END_DATE}],
+                'dimensions': [{'name': name} for name in DIMS],
+                'metrics': [{'expression': exp} for exp in METRICS]
+            }]
 
+    def get_report(self):
+        """Queries the Analytics Reporting API V4.
         Args:
             analytics: An authorized Analytics Reporting API V4 service object.
             body: Parameters for the report
         Returns:
             The Analytics Reporting API V4 response.
         """
-        self.analytics.reports().batchGet(body=body).execute()
+        return self.analytics.reports().batchGet(body=\
+            {'reportRequests':self.requests_list }).execute()
+
+
+    class binotel():
+        pass
+
+
+    class sql():
+        pass
 
 
 def main():
