3f37925793e7cef9dfe3838e856cf9240758defd
artemiroshnichenko <artemiroshnichenko@gmail.com>
2021-05-10 17:19:10 -0400

    GA report convert to data frame

--- get_data.py
+++ get_data.py
@@ -1,7 +1,7 @@
 import os 
 import mysql.connector as sql
 from dotenv import load_dotenv
-
+import pandas as pd
 
 # Google Analytics Reporting API V4
 from apiclient.discovery import build
@@ -35,6 +35,8 @@ class googleAnalitics():
         Returns:
             Request body
         """
+        self.DIMS = DIMS
+        self.METRICS = METRICS
         self.requests_list =  [{
                 'viewId': self.VIEW_ID,
                 'dateRanges': [{'startDate': START_DATE, 'endDate': END_DATE}],
@@ -48,10 +50,31 @@ class googleAnalitics():
             analytics: An authorized Analytics Reporting API V4 service object.
             body: Parameters for the report
         Returns:
-            The Analytics Reporting API V4 response.
+            The Analytics Reporting API V4 response json.
         """
-        return self.analytics.reports().batchGet(body=\
+        self.report = self.analytics.reports().batchGet(body=\
             {'reportRequests':self.requests_list }).execute()
+        return self.report
+
+    def convert_to_df(self):
+        """Convert json response to pandas data frame
+        Returns:
+            Report data
+        """
+        data_dic = {f"{i}": [] for i in self.DIMS + self.METRICS}
+        for report in self.report.get('reports', []):
+            rows = report.get('data', {}).get('rows', [])
+            for row in rows:
+                for i, key in enumerate(self.DIMS):
+                    data_dic[key].append(row.get('dimensions', [])[i])
+                dateRangeValues = row.get('metrics', [])
+                for values in dateRangeValues:
+                    all_values = values.get('values', [])
+                    for i, key in enumerate(self.METRICS):
+                        data_dic[key].append(all_values[i])
+        df = pd.DataFrame(data=data_dic)
+        df.columns = [col.split(':')[-1] for col in df.columns]
+        return df
 
 
     class binotel():
