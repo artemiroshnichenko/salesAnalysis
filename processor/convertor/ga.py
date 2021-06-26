edb1deb0fe2c667b39090cb8452f8fcac0509de2
artemiroshnichenko <artemiroshnichenko@gmail.com>
2021-06-26 04:34:05 -0400

    Add GA convertor

---
+++ processor/convertor/ga.py
@@ -0,0 +1,40 @@
+from numpy import source
+import pandas as pd
+import json
+from datetime import datetime
+
+
+class GAConvertor():
+
+    def __init__(self, raw, columns) -> None:
+        self.json = raw
+        self.data = pd.DataFrame()
+        self.columns = columns
+
+    def get_data(self):
+        if len(self.columns) == \
+            len(self.json['reports'][0]['data']['rows'][0]['dimensions']) +\
+                len(self.json['reports'][0]['data']['rows'][0]['metrics']):
+            for row in self.json['reports'][0]['data']['rows']:
+                self.data = self.data.append(self.get_data_from_row(row), ignore_index=True)
+        else:
+            print('Wrong count columns')
+        return self.data
+
+    def get_data_from_row(self, row):
+        __data = []
+        index_timestamp = self.json['reports'][0]['columnHeader']['dimensions'].index('ga:dateHourMinute')
+        for dimension in row['dimensions']:
+            if row['dimensions'][index_timestamp] == dimension:
+                __data.append(int(datetime.timestamp(datetime.strptime(dimension, '%Y%m%d%H%M'))))
+            else:
+                __data.append(dimension)
+        for metric in row['metrics']:
+            __data.append(metric['values'][0])
+        return pd.Series(__data, index=self.columns)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
